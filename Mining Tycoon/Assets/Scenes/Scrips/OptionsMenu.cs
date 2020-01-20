833771c3df20f9b028d3763613562781e7692334
XmantorCZ <otuna@seznam.cz>
2020-01-20 23:22:51 +0100

    MiningTycoon0.13v
    - Přidáno vyskakovací okno (Options)-
    - Přidáno tlačítko Save & exit
    - Slider Sound zatím nefunguje

---
+++ Mining Tycoon/Assets/Scenes/Scrips/OptionsMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public GameObject OptionPanel;
+    
+
+
+
+    public void ClickTheButton()
+    {
+        OptionPanel.SetActive(true);
+    }
+
+    public void CloseThePanel()
+    {
+        OptionPanel.SetActive(false);
+    }
+
+
+}
---
+++ Mining Tycoon/Assets/Scenes/Scrips/Panel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panel : MonoBehaviour
+{
+    public GameObject PanelClose;
+
+    void Start()
+    {
+        PanelClose.SetActive(false);
+
+    }
+
+}

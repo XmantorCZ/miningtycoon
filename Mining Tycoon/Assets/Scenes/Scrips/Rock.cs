aa2e19000cc8445bb6ab4d3d61e6800ded26b093
XmantorCZ <otuna@seznam.cz>
2020-01-23 14:33:47 +0100

    MiningTycoon0.19v
     -Odstraněn Ban
    - Odstraněn Bug
    - Úprava dropů rud z kamene

--- Mining Tycoon/Assets/Scenes/Scrips/Rock.cs
+++ Mining Tycoon/Assets/Scenes/Scrips/Rock.cs
@@ -16,34 +16,33 @@ public class Rock : MonoBehaviour
 
     public void ClickTheButton()
     {
-        Input.GetKeyDown("space");
         GlobalClicks.ClicksCount += 1;
-        genChance = Random.Range(1, 100);
-        if(genChance < 40)
+        genChance = Random.Range(1, 1000);
+        if(genChance < 525)
         {
 
         }
-        if(genChance >= 40 && genChance < 65)
+        if(genChance >= 525 && genChance < 700)
         {
             GlobalCoal.CoalCount += 1;
             CoalAnimat.GetComponent<Animation>().Play("CoalAnimation");
         }
-        if(genChance >= 65 & genChance < 80)
+        if(genChance >= 700 & genChance < 825)
         {
             GlobalCopper.CopperCount += 1;
             CopperAnimat.GetComponent<Animation>().Play("CopperAnimation");
         }
-        if(genChance >= 80 && genChance < 90)
+        if(genChance >= 825 && genChance < 925)
         {
             GlobalIron.IronCount += 1;
             IronAnimat.GetComponent<Animation>().Play("IronAnimation");
         }
-        if(genChance >= 90 && genChance < 95)
+        if(genChance >= 925 && genChance < 950)
         {
             GlobalGold.GoldCount += 1;
             GoldAnimat.GetComponent<Animation>().Play("GoldAnimation");
         }
-        if (genChance >= 95)
+        if (genChance >= 950)
         {
             GlobalSilver.SilverCount += 1;
             SilverAnimat.GetComponent<Animation>().Play("SilverAnimation");
